ccce217a87c8b46d23a343f553cf967ae17cab35
Grzegorz Szerszeń <101059466+NadmorskaAlpaka@users.noreply.github.com>
2022-06-11 10:49:10 +0200

    getting data from API, loader

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 let buttonCountClick;
 let buttonLocalStorageCount;
+let usersData;
 let buttonCountHtml = document.querySelector(".btn-count");
 let modalHtml = document.querySelector(".modal");
 let backgroundModalHtml = document.querySelector(".modal--background");
+let tableData = document.querySelector(".table-data");
+let modalTable = document.querySelector(".modal-table");
+let loader = document.querySelector(".loader");
 
 if(!localStorage.getItem("count")){
     buttonCountClick = 0;
@@ -10,12 +14,18 @@ if(!localStorage.getItem("count")){
     buttonCountClick = localStorage.getItem("count");
 };
 
-const openModal = () => {
+const openModal = async () => {
    modalHtml.classList += " open-modal";
    backgroundModalHtml.classList += " modal--background-visible";
    buttonCountClick++;
    localStorage.setItem("count",buttonCountClick); 
    displayButtonCount();
+   loader.classList += " visible";
+   if(!usersData){
+       await getUsers();
+    }
+   modalTable.classList += " visible";
+   loader.classList.remove("visible");
 };
 
 window.addEventListener('mouseup', function(event){
@@ -47,3 +57,25 @@ const resetCout = () => {
     buttonCountHtml.textContent = 0;
     localStorage.setItem("count",0); 
 };
+
+const getUsers = async () =>{
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    usersData = await response.json();
+    tableData.innerHTML = usersData.map((user) => userHtml(user)).join('');
+}
+
+function userHtml(user){
+    return `
+        <tr>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>
+                ${user.address.city}
+                ${user.address.street}
+                ${user.address.suite} 
+            </td>
+            <td>${user.phone}</td>
+            <td>${user.company.name}</td>
+        </tr>`
+}
+
